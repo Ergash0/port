65dfc6b8ae52a078fed3e1ab5e42476fa0253cd9
Ergash0 <92915363+Ergash0@users.noreply.github.com>
2021-11-16 15:55:20 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <body bgcolor=white background="https://images.pexels.com/photos/235994/pexels-photo-235994.jpeg?cs=srgb&dl=pexels-pixabay-235994.jpg&fm=jpg">
+  <meta charset="utf-8">
+  <title>Круглые изображения</title>
+  <style>
+   .parent {
+  padding: 10px;
+}
+img {
+  position: relative;
+  top: 80%; 
+  left: 80%;
+  height: 20%;
+  width: 20%;
+}
+   .round {
+    border-radius: 10000px; /* Радиус скругления */
+    border: px solid green; /* Параметры рамки */
+    box-shadow: 0 0 7px #666; /* Параметры тени */
+   
+  </style>
+ </head>
+ <body>
+
+
+   <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK0VkwK80EHyJLOXdyeNVvHMSoSp24KbbDwA&usqp=CAU" alt="" width="220" height="200" class="round"></p>
+<p>Name.  :   Nurmuhammed    </p>
+<hr>
+<p>Surname:   Akynbekuulu    </p>
+<hr>
+<p>Age    :   17            </p>
+<hr>
+<p>Class  :.  Csc-20a.       </p>
+<hr>
+<p>Phone : 996709491919
+ <div class="container">
+      
+        <form class="modal-content animate" action="https://www.facebook.com/nurik.nurikov.524>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+      <button type="submit">Facebook</button>
+  
+     
+       
+      </label>
+    </div>
+
+</body>
+</html>
